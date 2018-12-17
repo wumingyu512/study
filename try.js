53f7557b8128cb146c80f9e7874c1aee2e797853
wumingyu512 <452201768@qq.com>
2018-12-17 21:49:05 +0800

    ..

--- try.js
+++ try.js
@@ -1,22 +1,10 @@
-function myname(){
-    var name=document.forms["form"]["name"].value;
-    var tname=typeof name;
-    if (name==null || name=="")
-    {alert("用户名为空");return false;}
-    else if (tname != 'string')
-    {alert("用户名不可用");return false;}
-}
-function mypwd(){
-    var pwd=document.forms["form"]["pwd"].value;
-    var rpwd=document.forms["form"]["rpwd"].value;
-    if (pwd != rpwd)
-    {
-        alert("密码不一致，请重新输入");
-        return false;
-    }
-}
-function phone(){}
-$(document).ready(function(){$("form").submit(function(){
-    alert("ok");
+function check(){
+    $(document).ready(function(){("form").submit(function(){
+        var name=document.forms["form"]["name"].value;
+        if (name==null || name=="")
+        {$("#name").css("border-color","red");
+            alert("用户名为空");return false;}
+        else {alert("ok!");}
+    });
 });
-});
+}
